1030b668f341ed306649b7468b6b3ab6632e54eb
Tich53 <rdouette@yahoo.fr>
2022-06-23 15:09:06 +0200

    challenge 9

--- form.php
+++ form.php
@@ -1,19 +1,79 @@
+<?php
+
+//if ($_SERVER['request_method'] === 'post')  { //voir pour majuscule à "post?
+
+    //$errors = array(); // initialise une variable de type tableau afin d'y intégrer toutes les erreurs éventuelles
+
+    //Vérification du formulaire:
+
+        // tous les champs doivent être rempli : 
+        // Ok via les champs required dans le formulaire
+        // via PHP:
+
+    //if (empty($_POST['user_surname'])) {
+    //    $error['user_surname']="Your surname can't be empty";
+        
+    //}
+    //if (empty($_POST['user_name'])) {
+    //    $error['user_name']="Your firstname can't be empty";
+        
+    //}
+    //if (empty($_POST['user_email'])) {
+    //    $error['user_email']="Your email can't be empty";
+        
+    //}
+    //if (empty($_POST['user_tel'])) {
+    //    $error['user_tel']="Your phone number can't be empty";
+        
+    //}
+
+    //if (empty($_POST['message'])) {
+    //    $error['user_message']="Your message can't be empty";
+    
+//}
+
+    //if (empty($errors)) {
+
+        //header('location: /thanks.php');
+        //exit();
+    //}
+
+
+
+//
+
+
+//}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire challenge 8 & 9</title>
+</head>
+<body>
+    
+
 <form action = "/thanks.php" method="post">
     <div>
         <label for="surname">Nom :</label>
-        <input type="text" id="surname" name="user_surname" required autofocus>
+        <input type="text" id="surname" name="user_surname" >
     </div>
     <div>
         <label for="name">Prénom :</label>
-        <input type="text" id="name" name="user_name" required>
+        <input type="text" id="name" name="user_name" >
     </div>
     <div>
         <label for="email">@ :</label>
-        <input type="email" id="email" name="user_email" required>
+        <input type="email" id="email" name="user_email" >
     </div>
     <div>
         <label for="tel">Tel :</label>
-        <input type="tel" id="tel" name="user_tel" required>
+        <input type="tel" id="tel" name="user_tel" >
     </div>
     <label for="subject">Sujet: </label>
     <SELECT name="subject" id="subject" size="1"> 
@@ -25,11 +85,16 @@
     </SELECT>
     <div>
         <label for="message">Message</label>
-        <textarea name="user_message" id="message" required></textarea>
+        <textarea name="user_message" id="message" ></textarea>
     </div>
     <div class="button">
         <button type="submit">Envoyer votre message</button>
     </div>
 </form>
+
+</body>
+</html>
+
+
 
 
--- thanks.php
+++ thanks.php
@@ -7,12 +7,44 @@ $user_email = $_POST['user_email'];
 $user_tel = $_POST['user_tel'];
 $user_message = $_POST['user_message'];
 
-echo "Merci $user_name $user_surname de nous avoir contacté à propos de '$subject'.";
-echo "<br>";
-echo "<br>";
-echo "Un de nos conseiller vous contactera soit à l’adresse $user_email ou par téléphone au $user_tel dans les plus brefs délais pour traiter votre demande :";
-echo "<br>";
-echo "<br>";
-echo $user_message;
+$errors = "";
+
+if (empty($user_name)) {
+        echo $errors = "Le champ 'prénom' est obligatoire";
+        echo '<br>';
+    } 
+if (empty($user_surname)) {
+        echo $errors = "Le champ 'nom' est obligatoire";
+        echo '<br>';
+    } 
+if (empty($subject)){
+        echo $errors = "L'objet' est obligatoire";
+        echo '<br>';
+    } 
+if (empty($user_email)) {
+        echo $errors = "Le champ 'email' est obligatoire";
+        echo '<br>';
+    }
+if (empty($user_tel)) {
+        echo $errors = "Le champ 'numéro de téléphone' est obligatoire";
+        echo '<br>';
+}
+if (empty($user_message)) {
+        echo $errors = "Votre message est vide";
+        echo '<br>';
+}
+if (empty($errors)) {
+
+    echo "Merci $user_name $user_surname de nous avoir contacté à propos de '$subject'.";
+    echo "<br>";
+    echo "<br>";
+    echo "Un de nos conseillers vous contactera soit à l’adresse $user_email ou par téléphone au $user_tel dans les plus brefs délais pour traiter votre demande :";
+    echo "<br>";
+    echo "<br>";
+    echo $user_message;
+
+}
+
+
 
 ?>
