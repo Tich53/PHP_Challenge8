54928231ad99ff073705ee817414962db5dc5026
Tich53 <rdouette@yahoo.fr>
2022-06-23 08:14:24 +0200

    add  [user_tel]

--- thanks.php
+++ thanks.php
@@ -3,7 +3,7 @@
 echo "Merci $_POST[user_name] $_POST[user_surname] de nous avoir contacté à propos de '$_POST[subject]'.";
 echo "<br>";
 echo "<br>";
-echo "Un de nos conseiller vous contactera soit à l’adresse $_POST[user_email] ou par téléphone au {téléphone} dans les plus brefs délais pour traiter votre demande : 
+echo "Un de nos conseiller vous contactera soit à l’adresse $_POST[user_email] ou par téléphone au $_POST[user_tel] dans les plus brefs délais pour traiter votre demande : 
 $_POST[user_message]";
 
 ?>
